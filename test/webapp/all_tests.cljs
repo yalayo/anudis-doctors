b4ad02055a487ce72fca79e5c04a18e49b75097c
Mylab <labs@onmycrowd.com>
2020-04-19 00:03:50 -0700

    Adding new test

--- test/webapp/all_tests.cljs
+++ test/webapp/all_tests.cljs
@@ -1,3 +1,4 @@
 (ns webapp.all-tests
   (:require [views.chat.shell-test]
-            [views.chat.search-test]))
+            [views.chat.search-test]
+            [views.chat.conversation-list-test]))
