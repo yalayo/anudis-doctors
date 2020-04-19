cee05eaf243c14015bfc1dc790c62190d0e1993d
Mylab <labs@onmycrowd.com>
2020-04-19 00:09:44 -0700

    Adding conversation list to shell component

--- src/views/chat/shell.cljs
+++ src/views/chat/shell.cljs
@@ -1,8 +1,10 @@
 (ns views.chat.shell
   (:require [re-frame.core :as re-frame]
-            [views.chat.search :refer [search-component]]))
+            [views.chat.search :refer [search-component]]
+            [views.chat.conversation-list :refer [conversation-list-component]]))
 
 (defn shell-component []
   [:div {:id "chat-container"}
-   [search-component]])
+   [search-component]
+   [conversation-list-component]])
 
